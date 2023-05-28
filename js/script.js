76b844dbf30fb8ef359d0ede44684e2e70177bf4
Rabia Yousuf <53699291+rabiayousuf187@users.noreply.github.com>
2023-05-28 09:46:00 +0500

    Chapter 52 conti.
    Add class using id then ClassName but did not override previous one

--- js/script.js
+++ js/script.js
@@ -4,7 +4,6 @@ console.log("Add Class Using JS by ID then ClassName");
 function hide(){
     console.log("hide Funcation Call");
     console.log( document.getElementById("img1"));
-   
-    
-    document.getElementById("img1").className = "hidden";
+
+    document.getElementById("img1").className += " resize";
 }
