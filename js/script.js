235656560ab39a17369e511b56a9ec4d27e1bf44
Rabia Yousuf <53699291+rabiayousuf187@users.noreply.github.com>
2023-05-28 10:25:51 +0500

    Chapter 54 Swapping images and setting classes
    save id in var then apply multi. property
    like className and src change

--- js/script.js
+++ js/script.js
@@ -14,11 +14,20 @@ function swap(){
     console.log( document.getElementById("img2"));
     document.getElementById("img2").className = "resize";
     document.getElementById("img2").src = "https://images3.alphacoders.com/276/276565.jpg";
-
+    
 }
 function swapout(id,src){
     console.log( document.getElementById(id));
     document.getElementById("img2").className = "resize";
     document.getElementById("img2").src = src;
+    
+}
+
+console.log("Chapter 54 Swapping images and setting classes save id in var then apply multi. propert");
+function show(){
+    let ele = document.getElementById("img3");
+    console.log(ele);
+    ele.className = "show";
+    ele.src = "https://i.pinimg.com/originals/dd/8b/bb/dd8bbbd4a27bc4daee48a1f8387957f2.png";
 
 }
