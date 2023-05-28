180d36b83668f904764455cf81f8483cfec8b0e6
Rabia Yousuf <53699291+rabiayousuf187@users.noreply.github.com>
2023-05-28 09:34:30 +0500

    Chapter 52 Manipulating images and text to onwards
    Add Class Using JS by ID then ClassName
    HIDE IMG ON CLICK USING ADD CLASS DISPLAY NONE

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
 console.log("Chapter 52 Manipulating images and text to onwards");
+console.log("Add Class Using JS by ID then ClassName");
+
+function hide(){
+    console.log("hide Funcation Call");
+    console.log( document.getElementById("img1"));
+   
+    
+    document.getElementById("img1").className = "hidden";
+}
