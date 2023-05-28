36f70a8281ea1d67f24b8c2d20f03fc755d1ba82
Rabia Yousuf <53699291+rabiayousuf187@users.noreply.github.com>
2023-05-28 10:08:53 +0500

    Chapter 53 Swapping images
     onMouseover and onMouseout with fun arg

--- js/script.js
+++ js/script.js
@@ -4,7 +4,21 @@ console.log("Add Class Using JS by ID then ClassName");
 function hide(){
     console.log("hide Funcation Call");
     console.log( document.getElementById("img1"));
-
+    
     // document.getElementById("img1").className = "hidden";
     document.getElementById("img1").className += " resize";
+}
+
+console.log("Chapter 53: Swapping images");
+function swap(){
+    console.log( document.getElementById("img2"));
+    document.getElementById("img2").className = "resize";
+    document.getElementById("img2").src = "https://images3.alphacoders.com/276/276565.jpg";
+
+}
+function swapout(id,src){
+    console.log( document.getElementById(id));
+    document.getElementById("img2").className = "resize";
+    document.getElementById("img2").src = src;
+
 }
